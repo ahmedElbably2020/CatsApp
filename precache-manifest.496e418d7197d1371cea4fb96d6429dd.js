46cc3b342258c8d47c9027169d17bcd44d819b68
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-07-01 19:08:36 +0200

    Updates

---
+++ precache-manifest.496e418d7197d1371cea4fb96d6429dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe244464caccdb86c5992efdda8f167f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f00ae0674681608f82b8",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "03bc7183e80f9eef5bdd",
+    "url": "/static/css/main.ca5fd883.chunk.css"
+  },
+  {
+    "revision": "f00ae0674681608f82b8",
+    "url": "/static/js/2.1f518a4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1f518a4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03bc7183e80f9eef5bdd",
+    "url": "/static/js/main.afec1154.chunk.js"
+  },
+  {
+    "revision": "7db27281c5315fa4add7",
+    "url": "/static/js/runtime-main.9c4a0e5e.js"
+  },
+  {
+    "revision": "c449987d673bc29c73600666b842e143",
+    "url": "/static/media/logo.c449987d.png"
+  }
+]);
