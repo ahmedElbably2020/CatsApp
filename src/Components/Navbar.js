f27345e1df27c5b067427f416ae9487d1eeb14a3
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-07-01 13:41:26 +0200

    Store Products In Local Storage

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -17,7 +17,7 @@ function Navbar(props) {
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className='container'>
                     <a className="navbar-brand">
-                        <img src={logo} style={{ width: '40px', height: '40px' }} />
+                        <img src={logo} style={{ width: '40px', height: '40px' }} alt='logo' />
                     </a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
--- src/Store/Store.js
+++ src/Store/Store.js
@@ -2,14 +2,33 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import Reducer from './reducers'
 import thunk from 'redux-thunk'
 
-const initialValues = {
-    cart: []
+// Load Products Data From Local Storage
+function loadState() {
+    try {
+        let state = localStorage.getItem('cart')
+
+        if (state !== null) {
+            return JSON.parse(state)
+        }
+    } catch (err) {
+
+    }
+
+    return {
+        cart: []
+    }
+
 }
 
+// Save Products Data To Local Storage
+function saveState(state) {
+    return localStorage.setItem('cart', JSON.stringify(state))
+}
 
-const store = createStore(Reducer, initialValues, compose(
+const store = createStore(Reducer, loadState(), compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
+store.subscribe(() => saveState(store.getState()))
 export default store
