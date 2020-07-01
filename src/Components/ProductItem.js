257876f09cd3968924b03f74bade36bf4798c6ad
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-07-01 15:58:14 +0200

    Style Products Page And Some Style

--- src/Components/ProductItem.js
+++ src/Components/ProductItem.js
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom'
 function ProductItem(props) {
     const { img, name, price, ProductId } = props
     return (
-        <div className="card my-3">
-            <img className="card-img-top" src={img} alt="Card image cap" />
+        <div className="card my-3 border-0">
+            <div className='img-container position-relative'>
+                <img className="card-img-top" src={img} alt="Card image cap" />
+            </div>
             <div className="card-body">
                 <h5 className="card-title">{name}</h5>
-                <p className="card-text">Price: {price}$</p>
-                <Link to={`/products/${ProductId}`} className="btn btn-primary">Buy</Link>
+                <p className="card-text" style={{ color: 'green', fontWeight: 'bold' }}>Price: {price}$</p>
+                <Link to={`/products/${ProductId}`} className="btn btn-primary rounded-pill px-4 position-relative overflow-hidden">Buy</Link>
             </div>
 
         </div>
--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -4,11 +4,11 @@ function Slider() {
     return (
         <div className='landing-page d-flex align-items-center position-relative'>
             <div>
-                <h1 className='text-white'>Hellow I Cats Market</h1>
+                <h1 className='text-white'>Hellow In Cats Market</h1>
                 <p className='mt-4 w-50'>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
                 </p>
-                <button className='btn mt-4 p-3'>Read More</button>
+                <button className='btn mt-4 p-2 rounded-pill'>Read More</button>
             </div>
         </div>
     )
